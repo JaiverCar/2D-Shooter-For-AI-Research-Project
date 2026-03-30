d7b061c5027cdc963bc55dfa5b8793b15de82cdd
JaiverCar <126114014+JaiverCar@users.noreply.github.com>
2026-03-29 23:37:21 -0700

    changed Node class to be private

--- Assets/Scripts/AStar.cs
+++ Assets/Scripts/AStar.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public unsafe class AStar : MonoBehaviour
 {
-    public enum ListType { NoList, OpenList, ClosedList };
+    private enum ListType { NoList, OpenList, ClosedList };
 
     private int currentIteration = 0;
 
-    public class Node
+    private class Node
     {
         Node parent;       // Parent
         Node[] neighbors; // An array of the nodes neighbors, 0 = N, 1 = NE, 2 = E, 3 = SE, 4 = S, 5 = SW, 6 = W, 7 = NW    
