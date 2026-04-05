4418df6eab94088519bc2fdb6022bb6b59fd7901
JaiverCar <126114014+JaiverCar@users.noreply.github.com>
2026-04-04 20:05:15 -0700

    Flock Controller
    Add different variables to the flock controller script to adjust how strong of an effect each behavior should affect the movement of the enemy
    - each variable has it's own slider that ranges from 0 to 100

--- Assets/Scripts/FlockController.cs
+++ Assets/Scripts/FlockController.cs
@@ -5,6 +5,23 @@ using UnityEngine.UI;
 
 public class FlockController : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    private float cohesionStrength = 0.0f;
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    private float separationStrength = 0.0f;
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    private float alignmentStrength = 0.0f;
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    private float wanderStrength = 0.0f;
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    private float tetherStrength = 1.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +45,26 @@ public class FlockController : MonoBehaviour
         {
             enemy.SetEchoRadius(echoRadiusSlider.value);
         }
+    }
+
+    public float GetCohesionStrength()
+    {
+        return cohesionStrength;
+    }
+    public float GetSeparationStrength()
+    {
+        return separationStrength;
+    }
+    public float GetAlignmentStrength()
+    {
+        return alignmentStrength;
+    }
+    public float GetWanderStrength()
+    {
+        return wanderStrength;
+    }
+    public float GetTetherStrength()
+    {
+        return tetherStrength;
     }
 }
